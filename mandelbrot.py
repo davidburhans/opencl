b2277c6b7a04c4874b8b8e61225bba4e7ab8baab
David Burhans <david.burhans@gmail.com>
2016-05-01 22:03:14 -0600

    render to in-memory png

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,25 +3,44 @@ import pyopencl as cl
 from matplotlib import pyplot as plt
 from matplotlib import colors
 
+
 class Mandelbrot:
     xmin = -2.0
     xmax = 0.5
     ymin = -1.25
     ymax = 1.25
-    max_x_diff = 2.5
-    max_y_diff = 2.5
+    max_x_diff = xmax - xmin
+    max_y_diff = ymax - ymin
     width = 600
     height = 600
     dpi = 72
 
-    def __init__(self, interactive=True):
+    @property
+    def zoom_percent(self):
+        return self.max_x_diff / (self.xmax - self.xmin)
+
+    @property
+    def maxiter(self):
+        return min(np.sqrt(self.zoom_percent * 2) * 80, 4000)
+
+    def __init__(self, interactive=True, **viewport_kwargs):
         self.ctx = cl.create_some_context(interactive=interactive)
+        self._init_viewport(**viewport_kwargs)
 
     def _init_viewport(self, **kwargs):
-        self.xmin = kwargs.get('xmin', self.xmin)
-        self.xmax = kwargs.get('xmax', self.xmax)
-        self.ymin = kwargs.get('ymin', self.ymin)
-        self.ymax = kwargs.get('ymax', self.ymax)
+        if 'xcenter' not in kwargs:
+            self.xmin = kwargs.get('xmin', self.xmin)
+            self.xmax = kwargs.get('xmax', self.xmax)
+            self.ymin = kwargs.get('ymin', self.ymin)
+            self.ymax = kwargs.get('ymax', self.ymax)
+        else:
+            xcenter = kwargs.get('xcenter')
+            ycenter = kwargs.get('ycenter')
+            zoom_factor = kwargs.get('zoom_factor', 1)
+            self.xmin = xcenter - (self.max_x_diff / zoom_factor) / 2
+            self.xmax = xcenter + (self.max_x_diff / zoom_factor) / 2
+            self.ymin = ycenter - (self.max_y_diff / zoom_factor) / 2
+            self.ymax = ycenter + (self.max_y_diff / zoom_factor) / 2
         self.width = kwargs.get('width', self.width)
         self.height = kwargs.get('height', self.height)
         self.dpi = kwargs.get('dpi', self.dpi)
@@ -102,14 +121,14 @@ class Mandelbrot:
         ax = plt.axes()
         fig.add_axes(ax)
 
-        ticks = np.arange(0, self.width + 1, self.width / 5)
-        x_ticks = self.xmin + (self.xmax - self.xmin) * ticks / self.width
-        plt.xticks(ticks, x_ticks)
-        y_ticks = self.ymin + (self.ymax - self.ymin) * ticks / self.height
-        plt.yticks(ticks, y_ticks)
+        # ticks = np.arange(0, self.width + 1, self.width / 5)
+        # x_ticks = self.xmin + (self.xmax - self.xmin) * ticks / self.width
+        # plt.xticks(ticks, x_ticks)
+        # y_ticks = self.ymin + (self.ymax - self.ymin) * ticks / self.height
+        # plt.yticks(ticks, y_ticks)
 
         norm = colors.PowerNorm(gamma)
-        plot = ax.imshow(z, interpolation="bicubic", cmap=cmap, norm=norm, origin='lower')
+        plot = ax.imshow(z, cmap=cmap, norm=norm, origin='lower')
         return fig
 
     def render(self, **kwargs):
@@ -126,14 +145,20 @@ class Mandelbrot:
         self.ymin = y - y_offset
         self.ymax = y + y_offset
 
-    @property
-    def zoom_percent(self):
-        return self.max_x_diff / (self.xmax - self.xmin)
-
-    @property
-    def maxiter(self):
-        return min(np.sqrt(self.zoom_percent * 2) * 80, 4000)
+    def to_png(self, **kwargs):
+        from io import BytesIO
+        buf = BytesIO()
+        fig = self.build_plot(**kwargs)
+        a = fig.gca()
+        a.set_frame_on(False)
+        a.set_xticks([])
+        a.set_yticks([])
+        fig.savefig(buf, bbox_inches='tight', pad_inches=0, mode='png')
+        buf.seek(0)
+        return buf
 
 if __name__ == '__main__':
-    m = Mandelbrot()
-    m.render()
+    m = Mandelbrot(interactive=False)
+    png = m.to_png()
+    with open('test.png', 'wb') as f:
+        f.write(png.getbuffer())
