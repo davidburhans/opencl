c233be9810275fe3c55fe78a9f881ab079d13296
David Burhans <david.burhans@gmail.com>
2016-05-04 01:07:46 -0600

    pseudo-reactive design

--- resources.py
+++ resources.py
@@ -4,6 +4,7 @@ def make_color_map_link(color_map):
 color_maps = ['Accent','Accent_r','Blues','Blues_r','BrBG','BrBG_r','BuGn','BuGn_r','BuPu','BuPu_r','CMRmap','CMRmap_r','Dark2','Dark2_r','GnBu','GnBu_r','Greens','Greens_r','Greys','Greys_r','OrRd','OrRd_r','Oranges','Oranges_r','PRGn','PRGn_r','Paired','Paired_r','Pastel1','Pastel1_r','Pastel2','Pastel2_r','PiYG','PiYG_r','PuBu','PuBuGn','PuBuGn_r','PuBu_r','PuOr','PuOr_r','PuRd','PuRd_r','Purples','Purples_r','RdBu','RdBu_r','RdGy','RdGy_r','RdPu','RdPu_r','RdYlBu','RdYlBu_r','RdYlGn','RdYlGn_r','Reds','Reds_r','Set1','Set1_r','Set2','Set2_r','Set3','Set3_r','Spectral','Spectral_r','Wistia','Wistia_r','YlGn','YlGnBu','YlGnBu_r','YlGn_r','YlOrBr','YlOrBr_r','YlOrRd','YlOrRd_r','afmhot','afmhot_r','autumn','autumn_r','binary','binary_r','bone','bone_r','brg','brg_r','bwr','bwr_r','cool','cool_r','coolwarm','coolwarm_r','copper','copper_r','cubehelix','cubehelix_r','flag','flag_r','gist_earth','gist_earth_r','gist_gray','gist_gray_r','gist_heat','gist_heat_r','gist_ncar','gist_ncar_r','gist_rainbow','gist_rainbow_r','gist_stern','gist_stern_r','gist_yarg','gist_yarg_r','gnuplot','gnuplot2','gnuplot2_r','gnuplot_r','gray','gray_r','hot','hot_r','hsv','hsv_r','inferno','inferno_r','jet','jet_r','magma','magma_r','nipy_spectral','nipy_spectral_r','ocean','ocean_r','pink','pink_r','plasma','plasma_r','prism','prism_r','rainbow','rainbow_r','seismic','seismic_r','spectral','spectral_r','spring','spring_r','summer','summer_r','terrain','terrain_r','viridis','viridis_r','winter','winter_r',]
 
 color_html = '''
+<div style="width: 200px;float:left">
 <h2>Color Maps</h2>
 <ul>
 '''
@@ -11,7 +12,7 @@ color_html = '''
 for color_map in color_maps:
     color_html += '<li>' + make_color_map_link(color_map) + '</li>\n'
 
-color_html += '</ul>'
+color_html += '</ul></div>'
 
 html = '''
 <!DOCTYPE html>
@@ -71,8 +72,8 @@ html = '''
 </script>
 </head>
 <body>
+<div style="float:left; padding-right:10px">
 <h1><a href="https://en.wikipedia.org/wiki/Mandelbrot_set">The Mandelbrot Set</a></h1>
-
 <figure style="width:{width}px">
     <figcaption>
     Zoom: {zoom_factor}x
@@ -90,6 +91,7 @@ html = '''
     <li>Mousewheel-down to zoom out.</li>
     <li>Select a color map from the list below to change colors.</li>
 </ul>
+</div>
 ''' + color_html + '''
 </body>
 </html>
